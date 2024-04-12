16194d7c13c528cb6b0b38b3504c674a01aa2d5b
Nafasov <nafasovnurxon18@gmail.com>
2024-04-12 19:49:26 +0500

    modified

--- app/article/urls.py
+++ app/article/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
+from .views import (
+    CategoryAPIView,
+    TagsAPIView,
+    ArticleAPIView,
+    ContentAPIView,
+    CommentAPIView
+    )
 
 app_name = 'article'
 
 urlpatterns = [
-
+    path('category/', CategoryAPIView.as_view(), name='category'),
+    path('tags/', TagsAPIView.as_view(), name='tags'),
+    path('article/', ArticleAPIView.as_view(), name='article'),
+    path('<int:article_id>/content/', ContentAPIView.as_view(), name='content'),
+    path('<int:article_id>/comment/', CommentAPIView.as_view(), name='comment'),
 ]
 
 
